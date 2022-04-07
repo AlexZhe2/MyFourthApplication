c4db3a85a9b040dc0f93a110b2e44d697b5556ab
79110 <alex.home.surgut@gmail.com>
2022-04-07 22:51:58 +0300

    five (work)

--- app/src/main/java/com/example/myfourthapplication/MainActivity.java
+++ app/src/main/java/com/example/myfourthapplication/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.myfourthapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton ImageButton_01;
+    private ImageButton ImageButton_02;
+    private ImageButton ImageButton_03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton_01 = findViewById(R.id.ImageButton_xml_01);
+        ImageButton_02 = findViewById(R.id.ImageButton_xml_02);
+        ImageButton_03 = findViewById(R.id.ImageButton_xml_03);
+
+        testFunction_01(ImageButton_01);
+        testFunction_01(ImageButton_02);
+        testFunction_01(ImageButton_03);
+
     }
+
+    public void testFunction_01(ImageButton value) {
+        value.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.hi_android, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
 }
