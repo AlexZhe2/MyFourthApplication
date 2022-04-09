70f0c0902a1eb91dde2e904b9009ab398e63e915
79110 <alex.home.surgut@gmail.com>
2022-04-09 18:03:04 +0300

    six-4 (work)

--- app/src/main/java/com/example/myfourthapplication/MainActivity.java
+++ app/src/main/java/com/example/myfourthapplication/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton ImageButton_01;
     private ImageButton ImageButton_02;
     private ImageButton ImageButton_03;
+    private ImageButton ImageButton_04;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
         ImageButton_01 = findViewById(R.id.ImageButton_xml_01);
         ImageButton_02 = findViewById(R.id.ImageButton_xml_02);
         ImageButton_03 = findViewById(R.id.ImageButton_xml_03);
+        ImageButton_04 = findViewById(R.id.ImageButton_xml_04);
 
         testFunction_01(ImageButton_01);
         testFunction_01(ImageButton_02);
         testFunction_01(ImageButton_03);
+        testFunction_01(ImageButton_04);
 
     }
 
