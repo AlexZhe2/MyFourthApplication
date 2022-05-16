339e1f97d3750d1bdd15a6052839c7410bc7d3b4
79110 <alex.home.surgut@gmail.com>
2022-05-16 22:05:30 +0300

    nine-3 (work)

--- app/src/main/java/com/example/myfourthapplication/MainActivity.java
+++ app/src/main/java/com/example/myfourthapplication/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout my_LinerLayout_01;
       private LinearLayout my_LinerLayout_Today_01;
       private LinearLayout my_LinerLayout_Tomorrow_01;
+      private LinearLayout my_LinerLayout_Week_01;
+      private LinearLayout my_LinerLayout_Month_01;
       private LinearLayout my_LinerLayout_Late_01;
 
     int j = 0;
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         my_LinerLayout_01 = findViewById(R.id.my_LinerLayout_xml_01);//присвоение ранее созданной переменной
         my_LinerLayout_Today_01 = findViewById(R.id.my_LinerLayout_Today_xml_01);//присвоение ранее созданной переменной
         my_LinerLayout_Tomorrow_01 = findViewById(R.id.my_LinerLayout_Tomorrow_xml_01);//присвоение ранее созданной переменной
+        my_LinerLayout_Week_01 = findViewById(R.id.my_LinerLayout_Week_xml_01);//присвоение ранее созданной переменной
+        my_LinerLayout_Month_01 = findViewById(R.id.my_LinerLayout_Month_xml_01);//присвоение ранее созданной переменной
         my_LinerLayout_Late_01 = findViewById(R.id.my_LinerLayout_Late_xml_01);//присвоение ранее созданной переменной
         // типа LinearLayout конкретного значения LinearLayout из XML файла
         ImageButton_01 = findViewById(R.id.ImageButton_xml_01);
@@ -411,11 +415,46 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(Calendar.MILLISECOND, 0);
 
                 current_date=calendar.getTime();
+
+// для НА НЕДЕЛЕ
+                SimpleDateFormat formatter_week= new SimpleDateFormat("w"); // класс для форматирования
+
+                Date data_obj_week = date_obj;
+                String num_of_current_week =formatter_week.format(current_date);
+                String num_of_obj_week =formatter_week.format(data_obj_week);
+
+                boolean eq_1=num_of_current_week.equals(num_of_obj_week);
+
+                System.out.println("======num_of_current_week==="+num_of_current_week);
+                System.out.println("======data_obj_week==="+num_of_obj_week);
+                System.out.println("======eq_1==="+eq_1);
+/////
+// для В ЭТОМ МЕСЯЦЕ
+                SimpleDateFormat formatter_month= new SimpleDateFormat("M"); // класс для форматирования
+
+                Date data_obj_month = date_obj;
+                String num_of_current_month =formatter_month.format(current_date);
+                String num_of_obj_month =formatter_month.format(data_obj_week);
+
+                boolean eq_2=num_of_current_week.equals(num_of_obj_week);
+
+                System.out.println("======num_of_current_month==="+num_of_current_month);
+                System.out.println("======data_obj_month==="+num_of_obj_month);
+                System.out.println("======eq_2==="+eq_2);
+/////
+
+
+
+
+
+
                 //добавляем к текущей дате 1 день
                 calendar.add(Calendar.DAY_OF_MONTH, 1);
                // Date current_date_plus_day=new Date();
                 Date current_date_plus_day=calendar.getTime();
               //  current_date_plus_day=calendar.getTime();
+
+
 
                 System.out.println("====current_date_plus_day==="+formatter.format(current_date_plus_day));
 
@@ -424,7 +463,9 @@ public class MainActivity extends AppCompatActivity {
 
               //  разделение по сегодня, завтра, на неделе, потом
               // запись о задаче должна быть только на одном layout иначе не работает
-                if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))){
+                if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                        (!(num_of_current_week.equals(num_of_obj_week)))&&
+                        (!(num_of_current_month.equals(num_of_obj_month)))){
                  //   my_LinerLayout_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
                     my_LinerLayout_Late_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
 
@@ -449,9 +490,25 @@ public class MainActivity extends AppCompatActivity {
                    my_LinerLayout_Tomorrow_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
                 System.out.println("!!!====current_date_plus_day===!!!"+formatter.format(current_date_plus_day));
 
-                    }
+                }
 
-//////// конец заполнение Layout сегодня
+                if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                        ((num_of_current_week.equals(num_of_obj_week)))){
+
+                    my_LinerLayout_Week_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                }
+
+                if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                        (!(num_of_current_week.equals(num_of_obj_week)))&&
+                        ((num_of_current_month.equals(num_of_obj_month)))){
+
+                    my_LinerLayout_Month_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                }
+
+
+
+
+//////// конец заполнение Layout сегодня, завтра, на неделе, в этом месяце потом
 
                 //    int age = query.getInt(1);
                 //     textView.append("Name: " + name + " Age: " + age + "\n");
@@ -517,6 +574,34 @@ public class MainActivity extends AppCompatActivity {
                     calendar.set(Calendar.MILLISECOND, 0);
 
                     current_date=calendar.getTime();
+
+                    // для НА НЕДЕЛЕ
+                    SimpleDateFormat formatter_wek= new SimpleDateFormat("w"); // класс для форматирования
+
+                    Date data_obj_week = date_obj;
+                    String num_of_current_week =formatter_wek.format(current_date);
+                    String num_of_obj_week =formatter_wek.format(data_obj_week);
+
+                    boolean eq_1=num_of_current_week.equals(num_of_obj_week);
+
+                    System.out.println("======num_of_current_week==="+num_of_current_week);
+                    System.out.println("======data_obj_week==="+num_of_obj_week);
+                    System.out.println("======eq_1==="+eq_1);
+                    /////
+// для В ЭТОМ МЕСЯЦЕ
+                    SimpleDateFormat formatter_month= new SimpleDateFormat("M"); // класс для форматирования
+
+                    Date data_obj_month = date_obj;
+                    String num_of_current_month =formatter_month.format(current_date);
+                    String num_of_obj_month =formatter_month.format(data_obj_week);
+
+                    boolean eq_2=num_of_current_week.equals(num_of_obj_week);
+
+                    System.out.println("======num_of_current_month==="+num_of_current_month);
+                    System.out.println("======data_obj_month==="+num_of_obj_month);
+                    System.out.println("======eq_2==="+eq_2);
+/////
+
                     //добавляем к текущей дате 1 день
                     calendar.add(Calendar.DAY_OF_MONTH, 1);
                     // Date current_date_plus_day=new Date();
@@ -528,9 +613,11 @@ public class MainActivity extends AppCompatActivity {
                     //  разделение по сегодня, завтра, на неделе, потом
                     // запись о задаче должна быть только на одном layout иначе не работает
                     // сделать для Потом отдельный Layout сейчас ложиться в общий
-                    if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))){
-                     //  my_LinerLayout_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
-                     my_LinerLayout_Late_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                    if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                            (!(num_of_current_week.equals(num_of_obj_week)))&&
+                            (!(num_of_current_month.equals(num_of_obj_month)))){
+                        //   my_LinerLayout_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                        my_LinerLayout_Late_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
 
                         System.out.println("========Date1 is after Date2======");
                     }
@@ -540,18 +627,40 @@ public class MainActivity extends AppCompatActivity {
 
                         System.out.println("=======Date1 is before Date2=======");
                         System.out.println("=======add to layoyt TODAY=======");
+
                     }
 
                     if(date_obj.equals(current_date)){
                         my_LinerLayout_Today_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+
+                        System.out.println("======Date1 is equal Date2=====");
                     }
 
-                 //   if(date_obj.equals(current_date_plus_day)){
-                  //      my_LinerLayout_Tomorrow_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
-                 //       System.out.println("======Date1 is equal Date2=====");
-                //    }
+                    if(date_obj.equals(current_date_plus_day)){
+                        my_LinerLayout_Tomorrow_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                        System.out.println("!!!====current_date_plus_day===!!!"+formatter.format(current_date_plus_day));
 
-//////// конец заполнение Layout сегодня
+                    }
+
+                    if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                            ((num_of_current_week.equals(num_of_obj_week)))){
+
+                        my_LinerLayout_Week_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                    }
+
+                    if((date_obj.after(current_date))&&(!(date_obj.equals(current_date_plus_day)))&&
+                            (!(num_of_current_week.equals(num_of_obj_week)))&&
+                            ((num_of_current_month.equals(num_of_obj_month)))){
+
+                        my_LinerLayout_Month_01.addView(my_txtView_from_List_Two.get(j).getMy_linearLayout());
+                    }
+
+
+
+
+//////// конец заполнение Layout сегодня, завтра, на неделе, в этом месяце потом
+
+
                     //    int age = query.getInt(1);
                     //     textView.append("Name: " + name + " Age: " + age + "\n");
                     System.out.println("from second========================= " + j + " " + name);
